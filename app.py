2b814fde4725a1dd744e739dfb494a5c2def7270
Rahul Sai <noneuser2183@gmail.com>
2019-03-11 23:55:21 +0530

    new routes

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ from flask import Flask
 from pandas_datareader import data
 from textblob import TextBlob
 from tweepy import OAuthHandler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from keras.models import load_model
+from keras import backend
+import preprocessing
+import numpy as np
 
 app = Flask(__name__)
 
@@ -124,6 +130,52 @@ def findeverything(qurry):
 
 	return json.dumps(finobj)
 
+@app.route("/stockpr/apple")
+def predictstcg():
+    model = load_model('stockmodel.h5')
+    dataset = pd.read_csv("AAPL.csv", usecols=[1,2,3,4])
+    dataset = dataset.reindex(index = dataset.index[::-1])
+
+    # CREATING OWN INDEX FOR FLEXIBILITY
+    obs = np.arange(1, len(dataset) + 1, 1)
+
+    # TAKING DIFFERENT INDICATORS FOR PREDICTION
+    OHLC_avg = dataset.mean(axis = 1)
+    HLC_avg = dataset[['High', 'Low', 'Close']].mean(axis = 1)
+    close_val = dataset[['Close']]
+
+    # PREPARATION OF TIME SERIES DATASE
+    OHLC_avg = np.reshape(OHLC_avg.values, (len(OHLC_avg),1)) # 1664
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    OHLC_avg = scaler.fit_transform(OHLC_avg)
+
+    # TRAIN-TEST SPLIT
+    train_OHLC = int(len(OHLC_avg) * 0.75)
+    test_OHLC = len(OHLC_avg) - train_OHLC
+    train_OHLC, test_OHLC = OHLC_avg[0:train_OHLC,:], OHLC_avg[train_OHLC:len(OHLC_avg),:]
+
+    # TIME-SERIES DATASET (FOR TIME T, VALUES FOR TIME T+1)
+    trainX, trainY = preprocessing.new_dataset(train_OHLC, 1)
+    testX, testY = preprocessing.new_dataset(test_OHLC, 1)
+
+    # RESHAPING TRAIN AND TEST DATA
+    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+    step_size = 1
+
+    # PREDICTION
+    trainPredict = model.predict(trainX)
+    testPredict = model.predict(testX)
+    # DE-NORMALIZING FOR PLOTTING
+    trainPredict = scaler.inverse_transform(trainPredict)
+    trainY = scaler.inverse_transform([trainY])
+    testPredict = scaler.inverse_transform(testPredict)
+    testY = scaler.inverse_transform([testY])
+    last_val = testPredict[-1]
+    last_val_scaled = last_val/last_val
+    next_val = model.predict(np.reshape(last_val_scaled, (1,1,1)))
+    backend.clear_session()
+    return str(np.asscalar(last_val-5))
 
 if __name__ == '__main__':
     app.run(debug=True)
